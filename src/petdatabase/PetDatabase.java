165d7b411ce0684a898d78a3472f0ed368a00185
Sam Sobotka <sobotkas@csp.edu>
2023-05-12 20:22:42 -0500

    Release 2: Added ability to search for pets by name and age

--- src/petdatabase/PetDatabase.java
+++ src/petdatabase/PetDatabase.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class PetDatabase {
 
+    private static final String ALL = "all";
+
     public static void main(String[] args) {
 
         System.out.println("Pet Database Program\n");
@@ -15,15 +17,19 @@ public class PetDatabase {
         Scanner sc = new Scanner(System.in);
         ArrayList<String[]> pets = new ArrayList<>();
 
+        int numPetsAdded = 0;
+
         while (runLoop) {
 
             System.out.println("What would you like to do?");
             System.out.println("1) View all pets");
             System.out.println("2) Add more pets");
-            System.out.println("3) Exit program");
+            System.out.println("3) Search pets by name");
+            System.out.println("4) Search pets by age");
+            System.out.println("5) Exit program");
             System.out.print("Your choice: ");
-            choice = sc.nextInt();
-            sc.nextLine();
+            choice = sc.nextInt(); sc.nextLine();
+            System.out.println();
 
             switch (choice) {
 
@@ -35,10 +41,27 @@ public class PetDatabase {
 
                         System.out.print("Add pet (name, age): ");
                         String data = sc.nextLine();
-                        if (Objects.equals(data, "done")) break;
-                        else pets.add(data.split(" "));
+                        if (Objects.equals(data, "done")) {
+                            System.out.printf("%d pet(s) added.\n", numPetsAdded);
+                            numPetsAdded = 0;
+                            break;
+                        }
+                        else {
+                            pets.add(data.split(" "));
+                            numPetsAdded++;
+                        }
 
                     }
+                    break;
+                case 3:
+                    System.out.print("Enter a name to search: ");
+                    String name = sc.nextLine();
+                    displayPetsByName(pets, name);
+                    break;
+                case 4:
+                    System.out.print("Enter an age to search: ");
+                    int age = sc.nextInt(); sc.nextLine();
+                    displayPetsByAge(pets, age);
                     break;
                 default:
                     runLoop = false;
@@ -51,15 +74,58 @@ public class PetDatabase {
 
     private static void displayPets(ArrayList<String[]> pets) {
 
+        displayHeader();
+
+        for (int i = 0; i < pets.size(); i++)
+            displayRow(pets, i);
+
+        displayFooter(pets.size());
+
+    }
+
+    private static void displayPetsByName(ArrayList<String[]> pets, String name) {
+
+        displayHeader();
+
+        boolean validName;
+        for (int i = 0; i < pets.size(); i++) {
+            validName = pets.get(i)[0].equalsIgnoreCase(name) || name.equals(ALL);
+            if (validName)
+                displayRow(pets, i);
+        }
+
+        displayFooter(pets.size());
+
+    }
+
+    private static void displayPetsByAge(ArrayList<String[]> pets, int age) {
+
+        displayHeader();
+
+        boolean validAge;
+        for (int i = 0; i < pets.size(); i++) {
+            validAge = pets.get(i)[1].equals(String.valueOf(age));
+            if (validAge)
+                displayRow(pets, i);
+        }
+
+        displayFooter(pets.size());
+
+    }
+
+    private static void displayRow(ArrayList<String[]> pets, int i) {
+        System.out.printf("| %2d | %-9s | %3s |\n", i, pets.get(i)[0], pets.get(i)[1]);
+    }
+
+    private static void displayHeader() {
         System.out.println("+----------------------+");
         System.out.println("| ID | NAME      | AGE |");
         System.out.println("+----------------------+");
+    }
 
-        for (int i = 0; i < pets.size(); i++)
-            System.out.printf("| %2d | %-9s | %3s |\n", i, pets.get(i)[0], pets.get(i)[1]);
-
+    private static void displayFooter(int rowsInSet) {
         System.out.println("+----------------------+");
-
+        System.out.printf("%d rows in set.\n", rowsInSet);
     }
 
 }
