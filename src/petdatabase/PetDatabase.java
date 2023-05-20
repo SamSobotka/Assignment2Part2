d174da84b04d85ad90486935c0c1b4be83a13387
Sam Sobotka <sobotkas@csp.edu>
2023-05-20 15:49:22 -0500

    Added saving pet database to file

--- src/petdatabase/PetDatabase.java
+++ src/petdatabase/PetDatabase.java
@@ -1,5 +1,6 @@
 package petdatabase;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
@@ -12,12 +13,26 @@ public class PetDatabase {
 
         System.out.println("Pet Database Program");
 
+        String filename = "petDatabase.bin";
+
         boolean runLoop = true;
         int choice;
         Scanner sc = new Scanner(System.in);
-        ArrayList<String[]> pets = new ArrayList<>();
+        ArrayList<Pet> pets = new ArrayList<>();
 
         int numPetsAdded = 0;
+
+        try (
+                FileInputStream file = new FileInputStream(filename);
+                ObjectInputStream input = new ObjectInputStream(file)
+        ) {
+
+            pets = (ArrayList<Pet>) input.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error reading file; has " + filename + " been created yet?");
+            System.out.println(e.getMessage());
+        }
 
         while (runLoop) {
 
@@ -51,7 +66,8 @@ public class PetDatabase {
                             break;
                         }
                         else {
-                            pets.add(data.split(" "));
+                            String[] pet = data.split(" ");
+                            pets.add(new Pet(pet[0], Integer.parseInt(pet[1])));
                             numPetsAdded++;
                         }
 
@@ -78,12 +94,14 @@ public class PetDatabase {
 
                     System.out.print("Enter the ID of the pet you want to update: ");
                     int idToUpdate = sc.nextInt(); sc.nextLine();
-                    String oldPetInfo = pets.get(idToUpdate)[0] + " " + pets.get(idToUpdate)[1];
+                    String oldPetInfo = pets.get(idToUpdate).getName() + " " + pets.get(idToUpdate).getAge();
 
                     System.out.print("Enter the new name and age: ");
                     String newPetInfo = sc.nextLine();
+                    String[] newPet = newPetInfo.split(" ");
 
-                    pets.set(idToUpdate, newPetInfo.split(" "));
+                    pets.get(idToUpdate).setName(newPet[0]);
+                    pets.get(idToUpdate).setAge(Integer.parseInt(newPet[1]));
 
                     System.out.printf("%s has been changed to %s.\n", oldPetInfo, newPetInfo);
                     break;
@@ -94,7 +112,7 @@ public class PetDatabase {
 
                     System.out.print("Enter the ID of the pet you want to remove: ");
                     int idToRemove = sc.nextInt(); sc.nextLine();
-                    String removedPetInfo = pets.get(idToRemove)[0] + " " + pets.get(idToRemove)[1];
+                    String removedPetInfo = pets.get(idToRemove).getName() + " " + pets.get(idToRemove).getAge();
 
                     pets.remove(idToRemove);
 
@@ -108,13 +126,25 @@ public class PetDatabase {
 
         }
 
+        try (
+                FileOutputStream file = new FileOutputStream(filename);
+                ObjectOutputStream output = new ObjectOutputStream(file)
+        ) {
+
+            output.writeObject(pets);
+
+        } catch (IOException e) {
+            System.out.println("Error: database not saved");
+            e.printStackTrace();
+        }
+
     }
 
     private static void printBlankLine() {
         System.out.println();
     }
 
-    private static void displayPets(ArrayList<String[]> pets) {
+    private static void displayPets(ArrayList<Pet> pets) {
 
         displayHeader();
 
@@ -125,14 +155,14 @@ public class PetDatabase {
 
     }
 
-    private static void displayPetsByName(ArrayList<String[]> pets, String name) {
+    private static void displayPetsByName(ArrayList<Pet> pets, String name) {
 
         displayHeader();
 
         boolean validName;
         int petsDisplayed = 0;
         for (int i = 0; i < pets.size(); i++) {
-            validName = pets.get(i)[0].equalsIgnoreCase(name) || name.equals(ALL);
+            validName = pets.get(i).getName().equalsIgnoreCase(name) || name.equals(ALL);
             if (validName) {
                 displayRow(pets, i);
                 petsDisplayed++;
@@ -143,14 +173,14 @@ public class PetDatabase {
 
     }
 
-    private static void displayPetsByAge(ArrayList<String[]> pets, int age) {
+    private static void displayPetsByAge(ArrayList<Pet> pets, int age) {
 
         displayHeader();
 
         boolean validAge;
         int petsDisplayed = 0;
         for (int i = 0; i < pets.size(); i++) {
-            validAge = pets.get(i)[1].equals(String.valueOf(age));
+            validAge = pets.get(i).getAge() == age;
             if (validAge) {
                 displayRow(pets, i);
                 petsDisplayed++;
@@ -161,8 +191,8 @@ public class PetDatabase {
 
     }
 
-    private static void displayRow(ArrayList<String[]> pets, int i) {
-        System.out.printf("| %2d | %-9s | %3s |\n", i, pets.get(i)[0], pets.get(i)[1]);
+    private static void displayRow(ArrayList<Pet> pets, int i) {
+        System.out.printf("| %2d | %-9s | %3s |\n", i, pets.get(i).getName(), pets.get(i).getAge());
     }
 
     private static void displayHeader() {
